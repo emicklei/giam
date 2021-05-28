ff791e81e2a206c64bffac92d8322d5c83ec20a3
Ernest Micklei <ernest.micklei@gmail.com>
2021-05-28 14:52:14 +0200

    add owner listing

--- main.go
+++ main.go
@@ -13,11 +13,21 @@ func main() {
 	flag.Parse()
 	snapshot := loadMemberWithRoleInProjects()
 
-	//temp
-	member := os.Args[2]
-	for _, each := range snapshot {
-		if strings.HasSuffix(each.Member, member) {
-			fmt.Println(each.Member, each.ProjectName, each.Role)
+	if "roles" == os.Args[1] {
+		//temp
+		member := os.Args[2]
+		for _, each := range snapshot {
+			if strings.HasSuffix(each.Member, member) {
+				fmt.Println(each.Member, each.ProjectName, each.Role)
+			}
+		}
+	}
+
+	if "owners" == os.Args[1] {
+		for _, each := range snapshot {
+			if each.Role == "roles/owner" {
+				fmt.Println(each.Member, each.ProjectName, each.Role)
+			}
 		}
 	}
 }
